a2f8f80c83d56bd29fdeb6dc7596e5022216bb75
DESKTOP-9ORPSEF\Sandeep <melamsandeep@gmail.com>
2023-07-07 17:12:26 -0700

    first commit

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+	
+	    @RequestMapping("/hello")
+	    @ResponseBody
+	    public String helloGFG()
+	    {
+	        return "Hello GeeksForGeeks";
+	    }
+
+}
